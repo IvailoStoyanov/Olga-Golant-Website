0a9cabc1d901906965ec1a1b11410898401f671c
Ivaylo Stoyanov Stoyanov <ivailo.stoyanov.work@gmail.com>
2021-01-05 21:00:31 +0200

    fix navigation gradient

--- nextjs-olga-website/components/navigation/MainNavigation.tsx
+++ nextjs-olga-website/components/navigation/MainNavigation.tsx
@@ -11,8 +11,8 @@ class MainNavigation extends React.Component<{}, { isScrolled: boolean }> {
     this.handleScroll = this.handleScroll.bind(this);
   }
 
-  handleScroll(e: any) {
-    if (e.path[1].scrollY > 100) {
+  handleScroll() {
+    if (window.pageYOffset > 100) {
       this.setState(() => ({
         isScrolled: true,
       }));
